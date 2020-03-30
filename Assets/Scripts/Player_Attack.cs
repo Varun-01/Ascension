b2a2876d98e0717f556e533156abd420dd7252b0
stama <45017106+recarm@users.noreply.github.com>
2020-03-30 01:03:14 -0700

    Created Player_Attack Script
    Added basic attacks.

---
+++ Assets/Scripts/Player_Attack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Attack : MonoBehaviour
+{
+   
+    private CapsuleCollider col;
+    private Rigidbody rb;
+    private float orgColHight;
+    private Vector3 orgVectColCenter;
+    private Animator anim;
+    private AnimatorStateInfo currentBaseState;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        col = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        orgColHight = col.height;
+        orgVectColCenter = col.center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U)) {
+            anim.SetTrigger("LightPunch");
+        } else if (Input.GetKeyDown(KeyCode.I))
+            {
+                anim.SetTrigger("HeavyPunch");
+            }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            anim.SetTrigger("LightKick");
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            anim.SetTrigger("HeavyKick");
+        }
+
+    }
+}
