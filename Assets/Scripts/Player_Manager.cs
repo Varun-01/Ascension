a97f58befc61d029839ee7a237a7429c2656d6c7
stama <45017106+recarm@users.noreply.github.com>
2020-04-14 19:26:48 -0700

    Created Player_Manager.cs
    Added necessary variables.
    Implemented obtaining the player attack and defense from player_stats.cs.

---
+++ Assets/Scripts/Player_Manager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Manager : MonoBehaviour
+{
+    public int playerHealth = 1000;
+    public int winCount = 0;
+    public float stunTime = .5f;
+    public float knockDownTime = .5f;
+    public string currentState = "idle";
+    public float currentPosition;
+    public string characterName;
+    public string playerTag;
+    public int defenseStat;
+    public int attackStat;
+
+    [FMODUnity.EventRef]
+    public string PlayerStateEvent = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTag = gameObject.tag;
+        Debug.Log(playerTag);
+        Player_Stats playerStats = gameObject.GetComponent<Player_Stats>();
+        characterName = gameObject.name;
+        Debug.Log(characterName);
+        attackStat = playerStats.getPlayerAttack(characterName);
+        Debug.Log(attackStat);
+        defenseStat = playerStats.getPlayerDefense(characterName);
+        Debug.Log(defenseStat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void TakeDamage()
+    { 
+    
+    }
+
+    void GiveDamage()
+    {
+
+    }
+
+    void EndGame()
+    { 
+    
+    }
+
+    void AddWin() { 
+    
+    }
+}
