bd481e38c277881d3b0bc59e4135a58ff40d1bb8
stama <45017106+recarm@users.noreply.github.com>
2020-04-15 21:27:26 -0700

    Added Collider_Behavior.cs

---
+++ Assets/Scripts/Collider_Behavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider_Behavior : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate() {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag !="ground")
+        {
+            Debug.Log(collision.gameObject.tag);
+        }
+    }
+}
