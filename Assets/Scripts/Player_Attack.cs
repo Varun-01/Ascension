db0946bc9a7bc6c88776ab62a5cce6d0377df10a
Varun-01 <vallabhsura@gmail.com>
2020-04-15 21:35:20 -0700

    _

--- Assets/Scripts/Player_Attack.cs
+++ Assets/Scripts/Player_Attack.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Player_Attack : MonoBehaviour
 {
-   
+
     private CapsuleCollider col;
     private Rigidbody rb;
     private float orgColHight;
     private Vector3 orgVectColCenter;
     private Animator anim;
     private AnimatorStateInfo currentBaseState;
-    
+
     public Collider[] attackboxes;
     public bool hit = false;
     public float hit_damage;
@@ -28,9 +28,10 @@ public class Player_Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U)) {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
             LaunchAttack(attackboxes[0], "LightPunch");
-        } 
+        }
         else if (Input.GetKeyDown(KeyCode.I))
         {
             LaunchAttack(attackboxes[0], "HeavyPunch");
@@ -50,41 +51,16 @@ public class Player_Attack : MonoBehaviour
         // Launch the animation for the attack
         anim.SetTrigger(attackname);
         // Determine what came in contact with the attack
-        Collider[] cols = Physics.OverlapBox(co.bounds.center, co.bounds.extents, co.transform.rotation, LayerMask.GetMask("Hurtbox"), QueryTriggerInteraction.Collide);
+        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hurtbox"), QueryTriggerInteraction.Collide);
+
         foreach (Collider c in cols)
-        {
-            if (c.transform.root.name == transform.root.name)
             {
-                continue;
+                if (c.transform.root.name != transform.root.name)
+                {
+                    Debug.Log(c.name);
+                    Debug.Log(c.transform.root.name);
+                }
             }
-            else
-            {
-                Debug.Log(c.name);
-                hit = true;
-            }
-            
-        }
-        // Determine the damage
-        if (hit)
-        {
-            switch (attackname)
-            {
-                case "LightPunch":
-                    hit_damage = 10;
-                    break;
-                case "HeavyPunch":
-                    hit_damage = 20;
-                    break;
-                case "LightKick":
-                    hit_damage = 15;
-                    break;
-                case "HeavyKick":
-                    hit_damage = 30;
-                    break;
-                default:
-                    Debug.Log("Something's Wrong");
-                    break;
-            }
+            // Determine the damage
         }
     }
-}
