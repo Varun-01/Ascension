4e3ab24e1f3687250a0eaf696f6136d092432b1f
Varun-01 <vallabhsura@gmail.com>
2020-04-15 21:32:45 -0700

    Minor changes to Hitbox names

--- Assets/Scripts/Player_Attack.cs
+++ Assets/Scripts/Player_Attack.cs
@@ -13,6 +13,8 @@ public class Player_Attack : MonoBehaviour
     private AnimatorStateInfo currentBaseState;
     
     public Collider[] attackboxes;
+    public bool hit = false;
+    public float hit_damage;
 
     void Start()
     {
@@ -43,20 +45,46 @@ public class Player_Attack : MonoBehaviour
         }
 
     }
-    private void LaunchAttack(Collider col, string attackname)
+    private void LaunchAttack(Collider co, string attackname)
     {
         // Launch the animation for the attack
         anim.SetTrigger(attackname);
         // Determine what came in contact with the attack
-        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hurtbox"), QueryTriggerInteraction.Collide);
+        Collider[] cols = Physics.OverlapBox(co.bounds.center, co.bounds.extents, co.transform.rotation, LayerMask.GetMask("Hurtbox"), QueryTriggerInteraction.Collide);
         foreach (Collider c in cols)
         {
             if (c.transform.root.name == transform.root.name)
             {
                 continue;
             }
-            Debug.Log(c.name);
+            else
+            {
+                Debug.Log(c.name);
+                hit = true;
+            }
+            
         }
         // Determine the damage
+        if (hit)
+        {
+            switch (attackname)
+            {
+                case "LightPunch":
+                    hit_damage = 10;
+                    break;
+                case "HeavyPunch":
+                    hit_damage = 20;
+                    break;
+                case "LightKick":
+                    hit_damage = 15;
+                    break;
+                case "HeavyKick":
+                    hit_damage = 30;
+                    break;
+                default:
+                    Debug.Log("Something's Wrong");
+                    break;
+            }
+        }
     }
 }
