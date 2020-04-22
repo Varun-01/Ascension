99f1fa8643479475c919d0a05d67f114b7707486
stama <45017106+recarm@users.noreply.github.com>
2020-04-21 20:06:59 -0700

    Added Game_Manager.cs

---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public enum GameState { INTRO, MAIN_MENU }
+
+    GameObject player1 = GameObject.FindWithTag("Player1");
+    GameObject player2 = GameObject.FindWithTag("Player2");
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player_Manager player1Manager = player1.GetComponent<Player_Manager>();
+        Player_Manager player2Manager = player2.GetComponent<Player_Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
