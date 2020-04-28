71bf7d16f90f2982691af842aacee6a1e3df06fe
jessehurtado <jessehurtado1@gmail.com>
2020-04-27 20:33:41 -0700

    added 99 sec timer in game(no visual)

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -5,23 +5,63 @@ using UnityEngine;
 public class Game_Manager : MonoBehaviour
 {
     public enum GameState { INTRO, MAIN_MENU }
+    Vector3[] defaultPos1;
+    Vector3[] defaultScale1;
+    Quaternion[] defaultRot1;
+    Transform[] models1;
+    Vector3[] defaultPos2;
+    Vector3[] defaultScale2;
+    Quaternion[] defaultRot2;
+    Transform[] models2;
+    string p1 = "Player1";
+    string p2 = "Player2";
+    
 
-    GameObject player1 = GameObject.FindWithTag("Player1");
-    GameObject player2 = GameObject.FindWithTag("Player2");
-
+    //Attach Button from the Editor
+   // public Button resetButton;
+    //GameObject player1 = GameObject.FindWithTag("Player1");
+    //GameObject player2 = GameObject.FindWithTag("Player2");
+    public float x = 99.0f;
    
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject player1 = GameObject.FindWithTag(p1);
+        GameObject player2 = GameObject.FindWithTag(p2);
         Player_Manager player1Manager = player1.GetComponent<Player_Manager>();
         Player_Manager player2Manager = player2.GetComponent<Player_Manager>();
+     
+       
+        Invoke("ResetScene", x);
+     
+        
     }
-
+    void ResetScene()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
     // Update is called once per frame
     void Update()
     {
         
+        
+        
     }
-
+  
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
