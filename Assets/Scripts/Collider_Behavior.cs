2589e524bc3e612de20dbabc027f6379095da0f0
Varun-01 <vallabhsura@gmail.com>
2020-04-30 18:56:08 -0700

    Modified hitboxes

--- Assets/Scripts/Collider_Behavior.cs
+++ Assets/Scripts/Collider_Behavior.cs
@@ -5,32 +5,16 @@ using UnityEngine;
 
 public class Collider_Behavior : MonoBehaviour
 {
-    public float damage;
-    void Start()
-    {
-
-    }
-
-    void FixedUpdate() {
-        
-    }
-
-    void OnTriggerEnter(Collider collision)
+    public bool attacked = false;
+    public void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag =="Hurtbox")
         {
-            if (collision.gameObject.name == "Head")
-            {
-                Debug.Log("Head-Shot!");
-                damage = 20;
-                Debug.Log(damage);
-            }
-            else if (collision.gameObject.name == "Torso")
-            {
-                Debug.Log("Body-Shot!");
-                damage = 15;
-                Debug.Log(damage);
-            }
+            attacked = true;
         }
+    }
+    public void Hello()
+    {
+        Debug.Log("Hello");
     }
 }
--- Assets/Scripts/Player_Attack.cs
+++ Assets/Scripts/Player_Attack.cs
@@ -13,6 +13,9 @@ public class Player_Attack : MonoBehaviour
     public string characterName;
     public string playerTag;
     public Player_Manager playerManager;
+    public GameObject[] hitboxes;
+    public Collider_Behavior colliderBehavior;
+    public bool checker;
 
     Dictionary<string, int> attackValueTable = new Dictionary<string, int>();
 
@@ -31,6 +34,7 @@ public class Player_Attack : MonoBehaviour
     void Start()
     {
         playerManager = gameObject.GetComponent<Player_Manager>();
+        colliderBehavior = gameObject.GetComponent<Collider_Behavior>();
 
         playerState = FMODUnity.RuntimeManager.CreateInstance(PlayerStateEvent);
         playerState.start();
@@ -40,6 +44,10 @@ public class Player_Attack : MonoBehaviour
         orgColHight = col.height;
         orgVectColCenter = col.center;
         attack = FMODUnity.RuntimeManager.CreateInstance(AttackEvent);
+        foreach (GameObject box in hitboxes)
+        {
+            box.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -48,19 +56,30 @@ public class Player_Attack : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.U))
         {
             launchAttack("LightPunch");
+            StartCoroutine(StartAttack(1.40f, hitboxes[0]));
         }
         else if (Input.GetKeyDown(KeyCode.I))
         {
             launchAttack("HeavyPunch");
+            StartCoroutine(StartAttack(0.70f, hitboxes[0]));
         }
         else if (Input.GetKeyDown(KeyCode.O))
         {
             launchAttack("LightKick");
+            StartCoroutine(StartAttack(0.70f, hitboxes[1]));
         }
         else if (Input.GetKeyDown(KeyCode.P))
         {
             launchAttack("HeavyKick");
+            StartCoroutine(StartAttack(1.05f, hitboxes[2]));
         }
+    }
+    // co routine to activate the hitboxes only for the duration of the attack.
+    IEnumerator StartAttack(float attacktime, GameObject hit)
+    {
+        hit.SetActive(true);
+        yield return new WaitForSeconds(attacktime);
+        hit.SetActive(false);
     }
 
 
@@ -95,7 +114,8 @@ public class Player_Attack : MonoBehaviour
             anim.SetTrigger(attackName);
             attackSound(attackName);
             int damage = getAttackValue(attackName);
-            playerManager.GiveDamage(damage);
+            //playerManager.GiveDamage(damage);
+            //colliderBehavior.Hello();
         }
 
     }
