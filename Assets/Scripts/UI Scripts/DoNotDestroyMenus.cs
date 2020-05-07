d5925b7e72e504dcc4083cb18788270fd7e6c602
stama <45017106+recarm@users.noreply.github.com>
2020-05-07 00:43:12 -0700

    Fixed DontDestroyOnLoad... Again

---
+++ Assets/Scripts/UI Scripts/DoNotDestroyMenus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroyMenus: MonoBehaviour
+{
+    private static DoNotDestroyMenus mainCamera;
+    private static DoNotDestroyMenus selectionManager;
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("DoNotDestroy");
+
+        //if (objs.Length > 1)
+        //{
+        //  Destroy(this.gameObject);
+        //}
+
+        DontDestroyOnLoad(this.gameObject);
+        
+        if (this.gameObject.name == "Main Camera" && mainCamera == null)
+        {
+            mainCamera = this;
+        } else if (this.gameObject.name == "SelectionManager" && selectionManager == null)
+        {
+            selectionManager = this;
+        }else
+        {
+            DestroyObject(gameObject);
+        }
+    }
+}
